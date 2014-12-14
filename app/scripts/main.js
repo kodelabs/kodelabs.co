a5c16a24055950036debb22722c1c2153b97d9be
Stjepan <grgic.stjepan@gmail.com>
2014-12-14 21:39:50 +0100

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+/* jshint devel:true */
+console.log('\'Allo \'Allo!');
+
+
+// $(document).ready(function() {
+//   $('h1').jqFloat({
+//     width: 2000,
+//     height: 2000,
+//     speed: 50000
+//   });
+// });
